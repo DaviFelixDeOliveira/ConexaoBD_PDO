24012d3e6ba45eb3d174871bd81babca542bfa53
DaviFelixDeOliveira <daviFelixDeOliveira153@etec.sp.gov.br>
2025-06-05 13:23:38 -0300

    Terminar

--- uiverse/script.js
+++ uiverse/script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector(".form");
-const submit = document.querySelector(".enviar");
+const submit = document.getElementById("btnSubmit");
 const senha = document.getElementById("senha");
 const senhaConfirmacao = document.getElementById("senhaConfirma");
 
@@ -14,39 +14,43 @@ function checkSenha() {
     if (valorSenha === "") {
         console.warn("Preenchimento de senha é obrigatório.");
         return false;
+        submit.disabled = true;
     } else if (valorSenha.length < 8) {
         console.warn("A Senha precisa de no mínimo 8 caracteres.");
         return false;
-    }else if (!valorSenha.match(word)){
+    } else if (!valorSenha.match(word)) {
         console.warn("A senha precisa de pelo menos uma letra minúscula e maiúscula");
+        return false;
+        
     } else if (!valorSenha.match(number)) {
-           console.warn("A senha precisa conter pelo menos 1 número.");
-            return false;
-    }else if (!valorSenha.match(icon)) {
-            console.warn("A senha precisa conter pelo menos 1 caracter especial (!@#$_%&*).");
-            return false;
+        console.warn("A senha precisa conter pelo menos 1 número.");
+        return false;
+    } else if (!valorSenha.match(icon)) {
+        console.warn("A senha precisa conter pelo menos 1 caracter especial (!@#$_%&*).");
+        return false;
     } else if (valorSenhaConfirmacao === "") {
         console.warn("Confirmação de senha é obrigatória.");
         return false;
     } else if (valorSenhaConfirmacao !== valorSenha) {
         console.warn("As senhas não conferem. Tente novamente.");
         return false;
-    }else {
+    } else {
         return true;
     }
-   
 }
+
+
 
 function quandoInputModificado() {
     const senhaValida = checkSenha();
     console.log(senhaValida);
 
-    const valido = senhaValida;
-    if (valido) {
-        submit.disabled = false;
+    if (senhaValida) {
+        submit.disabled = true;  // Botão DESABILITADO para envio (senha inválida)
     } else {
-        submit.disabled = true;
+        submit.disabled = false;  // Botão HABILITADO para envio (senha válida)
     }
+    
 }
 
 for (const input of form.querySelectorAll("input")) {
