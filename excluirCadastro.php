6b7214674a0e2a44cabb31f6464f62599ac110b5
DaviFelixDeOliveira <davi.oliveira153@etec.sp.gov.br>
2025-06-26 13:25:02 -0300

    Adicionar Select

--- excluirCadastro.php
+++ excluirCadastro.php
@@ -1,23 +1,19 @@
     <button onclick="window.history.back()">Voltar</button>
 
-<?php
- 
-$nome =  $_POST['nm_usuario'];
-$senha =  $_POST['nm_senha'];
- 
- 
-    include 'conecta.php';
- 
-    $user = $conn->query("DELETE  FROM tb_usuario  WHERE nm_usuario ='".$nome."' AND nm_senha = '".$senha."' ;")->fetch();
-    if($user){
+    <?php
 
-        
-        echo "<pre>";
-        print_r($user);
-        echo "</pre>";
-    }else{
-        echo'Usuário excluído com sucesso!';
+    $nome =  $_POST['nm_usuario'];
+    $senha =  $_POST['nm_senha'];
+
+
+    include 'conecta.php';
+//FAZER SELEC PARA VERIFICAR SE O CADASTRO EXISTE
+    $user = $conn->query("DELETE  FROM tb_usuario  WHERE nm_usuario ='" . $nome . "' AND nm_senha = '" . $senha . "' ;")->fetch();
+    if ($user) {
+
+        echo "Usuário não encontrado!";
+    } else {
+        echo 'Usuário excluído com sucesso!';
     }
-   
-?>
- 
+
+    ?>
