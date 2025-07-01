39ef565f3e7367f9e533141fe59ebb270525877f
DaviFelixDeOliveira <davi.oliveira153@etec.sp.gov.br>
2025-07-01 15:50:10 -0300

    Projeto com tudo que o professor pediu. continuar no excluir e localizar cadastro e pensar no editar

--- bancoCadastro.sql
+++ bancoCadastro.sql
@@ -1,25 +1,29 @@
-drop database if exists bd_cadastro ;
+drop database if exists bd_cadastro;
+
 Create database if not exists bd_cadastro;
+
 use bd_cadastro;
 
-
-create table tb_usuario(
-	cd_usuario int auto_increment primary key,
+create table tb_usuario (
+    cd_usuario int auto_increment primary key,
     nm_usuario varchar(50) not null,
     nm_sobrenome varchar(50) not null,
     nm_email varchar(50) not null,
-    nm_login varchar (50),
+    nm_login varchar(50),
     nm_senha text,
     nr_fone varchar(15),
     img_foto blob
 );
 
+select
+    cd_usuario as Código,
+    concat (nm_usuario, ' ', nm_sobrenome) as "Nome Completo",
+    nm_email as "E-mail",
+    nm_login as Login,
+    nr_fone as Telefone
+    from    tb_usuario;
 
-
-select cd_usuario as Código, concat(nm_usuario, ' ', nm_sobrenome) as "Nome Completo", nm_email as "E-mail", nm_login as Login,
-nr_fone as Telefone  from tb_usuario ;	
-
-where nm_usuario and nm_senha;
+/*where nm_usuario and nm_senha;*/
 
 delete from tb_usuario
-where cd_usuario >=1;
+where cd_usuario >= 1;
